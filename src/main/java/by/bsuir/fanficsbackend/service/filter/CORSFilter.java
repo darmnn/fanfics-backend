690593bccf26331d7734a8c1305129282c9e2ab8
darmnn <darya.minina.11@gmail.com>
2022-05-04 19:48:27 +0300

    fix

--- src/main/java/by/bsuir/fanficsbackend/service/filter/CORSFilter.java
+++ src/main/java/by/bsuir/fanficsbackend/service/filter/CORSFilter.java
@@ -21,7 +21,7 @@ public class CORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-meб, Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization");
 
         filterChain.doFilter(servletRequest, servletResponse);
     }
